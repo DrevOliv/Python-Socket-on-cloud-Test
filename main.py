4bd90acc79e4a965ed7ca3118894211e6f282043
DrevOliv <110325775+DrevOliv@users.noreply.github.com>
2023-03-27 10:34:44 +0200

    dddd

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ PORT = 8080
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((HOST, PORT))
+server.bind(('', PORT))
 
 server.listen()
 
