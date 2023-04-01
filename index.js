6e2de76b737bb8fb0f8299f7eb2260f64c5180bd
DrevOliv <110325775+DrevOliv@users.noreply.github.com>
2023-04-01 14:25:07 +0200

    ddddddddddddd

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { express } = require('express');
+const Express  = require('express');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const server = express()
+const server = Express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
